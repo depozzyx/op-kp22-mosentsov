2bea5762922957870a41d9e360c384632d7c767e
depozzyx <mosentsowivan@gmail.com>
2022-11-07 10:27:59 +0200

    Basic setup

---
+++ sem_1_lab_1/Program.cs
@@ -0,0 +1,43 @@
+using System;
+
+public class Program
+{   
+    public static void Main()
+    {
+        Console.WriteLine("\nЗавдання #1 (варіант 14)");
+        Task1();
+
+        Console.WriteLine("\nЗавдання #2");
+        Task2();
+        
+        Console.WriteLine("\nЗавдання #3");
+        Task3();
+
+        Console.WriteLine("\nЗавдання #4");
+        Task4();
+    }
+
+
+    public static void Task1()
+    {
+
+    }
+
+
+    public static void Task2()
+    {
+
+    }
+
+
+    public static void Task3()
+    {
+
+    }
+
+
+    public static void Task4()
+    {
+
+    }
+}
