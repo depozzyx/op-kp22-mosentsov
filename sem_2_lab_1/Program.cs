526676639a43bca69c21af9ea18418b98524368f
depozzyx <mosentsowivan@gmail.com>
2023-03-05 10:23:07 +0200

    Intial setup

--- sem_2_lab_1/Program.cs
+++ sem_2_lab_1/Program.cs
@@ -1,8 +1,54 @@
 using System;
+using System.IO;
 
 class TextFiles {
     static void Main(String[] args)
     {
-        Console.WriteLine("hi");
+        Console.WriteLine("\nЗавдання #1");
+        Task1();
+
+        Console.WriteLine("\nЗавдання #2");
+        Task2();
+
+        Console.WriteLine("\nЗавдання #3");
+        Task3();
+
+        Console.WriteLine("\nЗавдання #4");
+        Task4();
+
+        Console.WriteLine("\nЗавдання #5");
+        Task5();
+
+        Console.WriteLine("\nЗавдання #6");
+        Task6();
+    }
+
+    static void Task1()
+    {
+    }
+
+    static void Task2()
+    {
+
+    }
+
+    static void Task3()
+    {
+
+    }
+
+    static void Task4()
+    {
+
+    }
+
+    static void Task5()
+    {
+
+    }
+
+    static void Task6()
+    {
+
     }
 }
