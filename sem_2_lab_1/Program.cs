f26740334bd995ba4cde3ca4786982e0d891277e
depozzyx <mosentsowivan@gmail.com>
2023-03-08 08:45:34 +0200

    Task4: Prototype

--- sem_2_lab_1/Program.cs
+++ sem_2_lab_1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 class TextFiles {
     static void Main(String[] args)
@@ -22,6 +23,27 @@ class TextFiles {
         Console.WriteLine("\nЗавдання #6");
         Task6();
     }
+    static private List<string> Split(string str, char delitimer)
+    {
+        List<string> result = new List<string>();
+
+        int fragmentStart = 0;
+        int fragmentNumber = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == delitimer) {
+                result.Add(str.Substring(fragmentStart, i - fragmentStart));
+                fragmentStart = i + 1;
+                fragmentNumber += 1;
+            }
+        }
+        if (fragmentStart != str.Length) {
+            result.Add(str.Substring(fragmentStart, str.Length - fragmentStart));
+        }
+
+        return result;
+    }
 
     /* Завдання #1 
      *
@@ -36,7 +58,7 @@ class TextFiles {
     {
         string text = "Перша строка\nДруга строка";
         Console.WriteLine(text);
-        File.WriteAllText("task1.txt", text);
+        File.WriteAllText("task1.txt", string.Join("--", Split(text, ' ')));
     }
 
     /* Завдання #2 
@@ -76,13 +98,61 @@ class TextFiles {
      */
     static void Task3()
     {
-        string readText = File.ReadAllText("input_task3.txt");
-        Console.WriteLine(readText); 
-    }
+        string allWords = File.ReadAllText("input_task3.txt");
+        List<string> splitted = Split(allWords, '\n');
 
+        bool isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 0; i < splitted.Count - 1; i++)
+            {
+                if (wordLess(splitted[i + 1], splitted[i])) {
+                    string temp = splitted[i];
+                    splitted[i] = splitted[i + 1];
+                    splitted[i + 1] = temp;
+
+                    isSorted = false;
+                }
+            }
+        }
+
+        foreach (var word in splitted)
+        {
+            Console.Write(word + " ");   
+        }
+        Console.WriteLine("\n---"); 
+
+    }
+    /*
+     * Допоміжна ф-ція для завдання 3. Перевіряє чи менше за алфавітом слово a ніж слово b
+     */
+    static private bool wordLess(string wordA, string wordB)
+    {   
+        wordA = wordA.ToLower();
+        wordB = wordB.ToLower();
+        int lengthA = wordA.Length;
+        int lengthB = wordB.Length;
+        for (int i = 0; i < Math.Min(lengthA, lengthB); i++)
+        {
+            if (wordA[i] < wordB[i]) return true;
+            else if (wordA[i] > wordB[i]) return false;
+        }
+
+        return lengthA < lengthB;
+    }
+    /* Завдання #4
+     *
+     */
     static void Task4()
     {
+        string csv = File.ReadAllText("input_task4.csv");
+        List<string> students = Split(csv, '\n');
 
+        foreach (var student in students)
+        {
+            List<string> info = Split(student, ",");
+        }
     }
 
     static void Task5()
